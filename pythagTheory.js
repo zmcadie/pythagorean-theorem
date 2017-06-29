19e5577786224ac5efad20bd4f00e770642ba2bd
Zola <zolamcadie@gmail.com>
2017-06-29 19:00:33 +0000

    when given an array [x, y] gives pythagorean theorem z

---
+++ pythagTheory.js
@@ -0,0 +1,12 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(object) {
+  return Math.sqrt((object.x * object.x) + (object.y * object.y))
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
